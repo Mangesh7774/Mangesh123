b7f7b77b1a97198a6330ec7ffecdd3766d6d89cd
mahadu123 <mahadumukir515@gmail.com>
2022-11-02 16:27:08 +0530

    New Line

--- Hibernate_Project_bankApplication/src/Admin/Test.java
+++ Hibernate_Project_bankApplication/src/Admin/Test.java
@@ -11,6 +11,7 @@ public class Test {
 	public static void main(String[] args) {
 		
 		System.out.println("Added for practise");
+		System.out.println("Added for practise By Mahadu");
 
 		Scanner sc = new Scanner(System.in); 
 		SBI sbi = new SBI();
